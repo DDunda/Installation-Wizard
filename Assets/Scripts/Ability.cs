a2f6322d2fcf35c28ec61ea0cf976886cd910c23
Aidan Davies <poppers@davies.net.au>
2023-04-30 19:13:22 +1000

    Created abstract class Ability

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+public abstract class Ability : MonoBehaviour
+{
+    public abstract bool Activate(); // called when the ability is activated: returns whether the ability successfully activated
+}
