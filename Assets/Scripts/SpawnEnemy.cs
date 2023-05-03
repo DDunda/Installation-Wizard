d1a58a9e7d57c6995df227d9b6179462887a12e0
JR14141414 <128942924+JR14141414@users.noreply.github.com>
2023-05-03 20:42:24 +1000

    Updates to SpawnEnemy.cs

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -1,58 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
-    //Enemies to Spawn
+    //Enemy Variables
     [SerializeField] private List<GameObject> enemies;
-    [SerializeField] private float spawnInterval = 3f;
-    [SerializeField] private float minSpawnInterval = 1f;
-    [SerializeField] private float maxSpawnInterval = 5f;
-    [SerializeField] private int maxSpawns = 5;
+    [SerializeField] private List<int> indexToSpawn;
+    private int currentIndex = 0;
+    [SerializeField] private int maximumEnemies;
+    private int enemiesSpawned;
 
-    //Spawning Area Characteristics
-    [SerializeField] private float height;
+    //Spawn Area Variables
     [SerializeField] private float width;
+    [SerializeField] private float height;
+    [SerializeField] private List<Transform> fixedSpawnLocations;
+    [SerializeField] private bool randomLocation;
 
-    //Object States
+
+    //Spawn Time Variables
+    [SerializeField] private float fixedSpawnDelay;
+    [SerializeField] private float minSpawnDelay;
+    [SerializeField] private float maxSpawnDelay;
     private bool canSpawn = true;
-    private int activeSpawns = 0;
+    [SerializeField] private bool randomInterval;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (canSpawn && activeSpawns < maxSpawns)
+        //Only Spawn an enemy if not on cooldown
+        if (canSpawn && enemiesSpawned < maximumEnemies)
         {
-            //StartCoroutine(RandomSpawn(enemies[0]));
-            StartCoroutine(StaticSpawn(enemies[0]));
+            //Checks if the list has reached the end and restarts it
+            if (currentIndex == indexToSpawn.Count)
+            {
+                currentIndex = 0;
+            }
+            //Spawn the enemy
+            StartCoroutine(Spawn(enemies[currentIndex]));
+
+            enemiesSpawned++;
+
+            //Move to the next enemy in the list
+            currentIndex++;
         }
     }
 
-    private IEnumerator RandomSpawn(GameObject enemy)
+    public IEnumerator Spawn(GameObject enemy)
     {
-        spawnInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
-        var spawnLocation = new Vector3(Random.Range(-0.5f * width, 0.5f * width), Random.Range(-0.5f * height, 0.5f * height));
-        Instantiate(enemy, spawnLocation, Quaternion.identity);
         canSpawn = false;
-        activeSpawns++;
-        yield return new WaitForSeconds(spawnInterval);
-        canSpawn = true;
-    }
+        var randDelay = Random.Range(minSpawnDelay, maxSpawnDelay);
+        //Checks if randomSpawnIntervals are required
+        var spawnDelay = randomInterval ? randDelay : fixedSpawnDelay;
 
-    private IEnumerator StaticSpawn(GameObject enemy)
-    {
-        var spawnLocation = new Vector3(Random.Range(-0.5f * width, 0.5f * width), Random.Range(-0.5f * height, 0.5f * height));
+        var randX = Random.Range(-0.5f * width, 0.5f * width);
+        var randY = Random.Range(-0.5f * height, 0.5f * height);
+        var randLocation = new Vector3 (randX, randY);
+
+        //Checks if the location is currently occupied
+        var objectsAtLocation = Physics2D.OverlapCircle(randLocation, 1.3f);
+
+        //Chooses a new location if the chose location has an object present
+        while (objectsAtLocation != null)
+        {
+            randX = Random.Range(-0.5f * width, 0.5f * width);
+            randY = Random.Range(-0.5f * height, 0.5f * height);
+            randLocation = new Vector3(randX, randY);
+            objectsAtLocation = Physics2D.OverlapCircle(randLocation, 1.3f);
+        }
+
+        var spawnLocation = randomLocation ? randLocation : fixedSpawnLocations[0].position;
+
+        //Spawn the enemy
         Instantiate(enemy, spawnLocation, Quaternion.identity);
-        canSpawn = false;
-        activeSpawns++;
-        yield return new WaitForSeconds(spawnInterval);
+
+        //wait for cooldown before spawning again
+        yield return new WaitForSeconds(spawnDelay);
+
         canSpawn = true;
     }
 }
