d8cac02374acbbc21a461c9a600b8cede459f8d0
Aidan Davies <poppers@davies.net.au>
2023-05-08 23:31:12 +1000

    Modified Art Spray ability to work with new Ability system

--- Assets/Scripts/ArtSprayAbility.cs
+++ Assets/Scripts/ArtSprayAbility.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class ArtSprayAbility : Ability
 {
 	[SerializeField]
+	private KeyCode button;
+	[SerializeField]
 	public JPEGWizStyle style;
 	[Min(0)] public float deadzone = 0.1f;
 	public Range<float> speed = 5;
@@ -27,44 +29,52 @@ public class ArtSprayAbility : Ability
 		projectileArray.AddEntry(1, projectilePrefab);
 	}
 
-	public override bool Activate(Transform entity)
+	public override bool Activate()
 	{
-		Vector2 direction = Extensions.GetMouseWorldPosition(Camera.main) - (Vector2)entity.position;
-		if (direction.magnitude <= deadzone) return false;
-
-		direction.Normalize();
-
-		float directionAngle = direction.Angle();
-
-		Vector2 v = Vector2.zero;
-		Rigidbody2D rb;
-		if(entity.TryGetComponent(out rb)) v = rb.velocity;
-
-		EntityTeams et;
-		Team t = 0;
-		if (entity.TryGetComponent(out et)) t = et.Teams;
-
-		var projectiles = ProjectileManager.SpawnProjectileRandomArc(
-			entity.position,
-			randomRotation,
-			v,
-			speed,
-			randomAngularSpeed,
-			t,
-			projectileArray,
-			n,
-			new Range<float>(directionAngle - spreadAngle / 2f, directionAngle + spreadAngle / 2f),
-			0,
-			directionRandomness,
-			fireRadius,
-			randomScale
-		);
-
-		foreach(var projectile in projectiles )
+		if (!OnCooldown && Input.GetKeyDown(button))
 		{
-			style.AddSprite(projectile.gameObject, projectile.GetComponent<CircleCollider2D>(), projectileSortingLayer);
+			Transform entity = transform; // the transform of the player
+
+			Vector2 direction = Extensions.GetMouseWorldPosition(Camera.main) - (Vector2)entity.position;
+			if (direction.magnitude <= deadzone) return false;
+
+			direction.Normalize();
+
+			float directionAngle = direction.Angle();
+
+			Vector2 v = Vector2.zero;
+			Rigidbody2D rb;
+			if (entity.TryGetComponent(out rb)) v = rb.velocity;
+
+			EntityTeams et;
+			Team t = 0;
+			if (entity.TryGetComponent(out et)) t = et.Teams;
+
+			var projectiles = ProjectileManager.SpawnProjectileRandomArc(
+				entity.position,
+				randomRotation,
+				v,
+				speed,
+				randomAngularSpeed,
+				t,
+				projectileArray,
+				n,
+				new Range<float>(directionAngle - spreadAngle / 2f, directionAngle + spreadAngle / 2f),
+				0,
+				directionRandomness,
+				fireRadius,
+				randomScale
+			);
+
+			foreach (var projectile in projectiles)
+			{
+				style.AddSprite(projectile.gameObject, projectile.GetComponent<CircleCollider2D>(), projectileSortingLayer);
+			}
+
+			RestartCooldown();
+			return true;
 		}
 
-		return true;
+		return false;
 	}
 }
