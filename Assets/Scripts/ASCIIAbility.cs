98f9f89577c9ea4a23088457c32f0a27e4a0630c
Aidan Davies <poppers@davies.net.au>
2023-05-09 16:12:30 +1000

    ASCII text trail now randomises text

--- Assets/Scripts/ASCIIAbility.cs
+++ Assets/Scripts/ASCIIAbility.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ASCIIAbility : Ability
 {
     [SerializeField]
     private KeyCode button;
 	[SerializeField]
-	private GameObject projectilePrefab;
+	private GameObject projectilePrefab; // the projectile to fire
+	[SerializeField]
+	private GameObject trailText; // the text to modify
+	[SerializeField]
+	private float textTimerMax = 0.2f; // every n seconds, change the text in projectile trails
+	private float textTimer;
+	[SerializeField]
+	private int trailCharNum = 10; // amount of characters to add into trail texture when randomising
 	[SerializeField]
 	private float projectileSpeed = 5.0f;
 	private Transform entity;
@@ -16,6 +24,7 @@ public class ASCIIAbility : Ability
 	private void Awake()
 	{
 		entity = transform;
+		textTimer = textTimerMax;
 	}
 
 	public override bool Activate()
@@ -43,7 +52,7 @@ public class ASCIIAbility : Ability
 
 			ProjectileManager.SpawnProjectile(
 				entity.position, 
-				angle,
+				0,
 				v,
 				0,
 				t,
@@ -51,6 +60,25 @@ public class ASCIIAbility : Ability
 
 			RestartCooldown();
 			return true;
+		}
+
+		// randomise the text inside the trail
+		textTimer -= Time.deltaTime;
+		if (textTimer < 0)
+		{
+			string newText = "";
+
+			// pick ten random characters from a set
+			for (var i = 0; i < trailCharNum; i++)
+			{
+				var code = Random.Range(33, 125);
+				newText += (char)code;
+			}
+
+			// set new trail text
+			trailText.GetComponent<TMP_Text>().text = newText;
+			// reset timer
+			textTimer += textTimerMax; 
 		}
 
 		return false;
