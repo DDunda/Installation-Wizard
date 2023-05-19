4d11063dff3dd9105b0c0f3925648b6e3e8a4a3c
R0binicus <102570571+R0binicus@users.noreply.github.com>
2023-05-19 13:16:45 +1000

    Made basic start menu and begun work on end screen

---
+++ Assets/Scripts/UI/StartMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour 
+{
+	public void LoadScene(string sceneName)
+	{
+		if (sceneName == null)
+			Debug.Log("<color=orange>"+gameObject.name+": No Scene Name Was given for LoadScene function!</color>");
+		SceneManager.LoadScene(sceneName); //load a scene
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+		Debug.Log("Quit!");
+	}
+	//not actually used but here when needed for easy access
+	//private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Escape)) {
+    //        //SceneManager.LoadScene("StartMenu");
+    //        Application.Quit();
+	//	    Debug.Log("Quit!");
+    //    }
+    //}
+}
