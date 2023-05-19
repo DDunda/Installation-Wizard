c857bd548bdcb23ddb2fdebfe8dee8ae1e3031f8
R0binicus <102570571+R0binicus@users.noreply.github.com>
2023-05-19 16:01:55 +1000

    Fixed level transition and added CustomEntityDeaths script

---
+++ Assets/Scripts/Entities/CustomEntityDeaths.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomEntityDeaths : MonoBehaviour
+{
+
+    public void PlayerDeath()
+	{
+        SceneManager.LoadScene("");
+	}
+
+    public void NextLevel()
+	{
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+    public void LoadScene(string sceneName)
+	{
+		if (sceneName == null)
+			Debug.Log("<color=orange>"+gameObject.name+": No Scene Name Was given for LoadScene function!</color>");
+		SceneManager.LoadScene(sceneName); //load a scene
+	}
+}
